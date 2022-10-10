7a1c5371fd19a06d5bc3935170662184ee304930
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-10-10 09:17:38 +0200

    samples review

--- load-policies.sh
+++ load-policies.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-conjur policy load -b root -f app.yaml
-conjur policy load -b root -f permit-us-replication.yaml
-conjur policy load -b root -f permit-eu-replication.yaml
+conjur policy load -b data -f myapp-host.yaml
+conjur policy load -b data/vault/bnl-secrets-safe -f grant_permissions.yaml
